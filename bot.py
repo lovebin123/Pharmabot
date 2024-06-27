864755042269ff66e25f573f498230fab0d69426
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-28 00:18:36 +0530

    changed to CSV Loader

--- bot.py
+++ bot.py
@@ -1,16 +1,38 @@
 from langchain_together import Together
-from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-import pandas as pd
 import os
-import re
-api_key=os.environ.get('TOGETHER_AI_API_KEY')
-llm=Together(together_api_key=api_key,
-             model='meta-llama/Meta-Llama-3-70B',
-             temperature=0.5,
-             max_tokens=512,
-             top_p=0.9,   )
-dfrm=pd.read_csv('a-b-c-d-e-f-g-h-i-j-k-l-m.csv')
-agent=create_pandas_dataframe_agent(llm=llm,df=dfrm,verbose=False,return_intermediate_steps=True,max_iterations=10,max_execution_time=60)
-answer=agent.invoke('What medicine is used to treat asthma?')
-intermediate_output=answer['intermediate_steps'][7][1]
-print(intermediate_output)
+from flask import Flask, request, Response
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import PromptTemplate
+from langchain_cohere import CohereEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv('TOGETHER_AI_API_KEY')
+cohere_api_key=os.getenv('COHERE_API_KEY')
+llm = Together(together_api_key=api_key,
+               model='meta-llama/Meta-Llama-3-70B',
+               temperature=0.3,
+               max_tokens=200,
+)
+loader = CSVLoader('a-b-c-d-e-f-g-h-i-j-k-l-m.csv',  encoding="utf-8")
+data = loader.load()
+embeddings = CohereEmbeddings(cohere_api_key=cohere_api_key,model='embed-english-v3.0',)
+db=FAISS.from_documents(data,embedding=embeddings)
+retriever=db.as_retriever()
+system_prompt = (
+    "You are a helpful assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know.Use three sentences maximum and keep the answer"
+    " precise"
+    "\n\n"
+    "{context}"
+)
+prompt = PromptTemplate.from_template(system_prompt)
+combine_docs_chain=create_stuff_documents_chain(llm=llm,prompt=prompt)
+chain=create_retrieval_chain(retriever=retriever,combine_docs_chain=combine_docs_chain)
+question = "Which medicine is used to treat diabetes?"
+response=chain.invoke({"input":question})
+print(response['answer'])
