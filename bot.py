b349921237de58886ff84adfc620cac2a9327211
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-29 20:54:30 +0530

    changed

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from langchain_together import Together
 import os
+import json
 from flask import Flask, request, Response
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain_core.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveJsonSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import PromptTemplate,ChatPromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv('TOGETHER_AI_API_KEY')
@@ -18,25 +18,30 @@ llm = Together(together_api_key=api_key,
                temperature=0.0,
                max_tokens=512,
 )
-loader = CSVLoader('a-b-c-d-e-f-g-h-i-j-k-l-m.csv',  encoding="utf-8",csv_args={
-        "delimiter": ","})
-
-data = loader.load()
-embeddings=GoogleGenerativeAIEmbeddings(google_api_key=google_api_key,model='models/text-embedding-004',task_type='clustering')
-db=FAISS.from_documents(data,embedding=embeddings,)
+loader=open('Medicines.json',encoding='utf-8',)
+data=json.load(loader)
+splitter=RecursiveJsonSplitter()
+splitted_docs=splitter.create_documents(texts=data)
+embeddings=GoogleGenerativeAIEmbeddings(google_api_key=google_api_key,model='models/text-embedding-004')
+db=Chroma.from_documents(splitted_docs,embedding=embeddings,)
 retriever=db.as_retriever()
-system_prompt = (
-    "You are a helpful AI assistant. "
-    "Make your response as accurate as possible. "
-    "Provide only the medicine names and do not provide uses or dosage information. "
-    "In order to query use data['Use'].contains()"
-    "{context}"
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
 )
-
-prompt = PromptTemplate.from_template(template=system_prompt)
-combine_docs_chain=create_stuff_documents_chain(llm=llm,prompt=prompt)
-chain=create_retrieval_chain(retriever=retriever,combine_docs_chain=combine_docs_chain)
-question = "Which medicine is used to treat diabetes?"
-
-response=chain.invoke({"input":question})
-print(response['answer'])
+question = "Which medicines are used to treat dermatitis ?"
+response=chain.invoke(question)
+index=response.find('`')
+if(index!=-1):
+    print(response[:index])
+else:
+    print(response)
