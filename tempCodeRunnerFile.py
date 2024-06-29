3693496653faeba0fecf257f5472f0fd35547c77
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-29 19:59:53 +0530

    added'

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -7,33 +7,34 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import PromptTemplate
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_cohere import CohereEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv('TOGETHER_AI_API_KEY')
 cohere_api_key=os.getenv('COHERE_API_KEY')
-google_api_key=os.getenv('GOOGLE_API_KEY')
 llm = Together(together_api_key=api_key,
                model='meta-llama/Meta-Llama-3-70B',
-               temperature=0.0,
-               max_tokens=512,
+               temperature=0.5,
+               max_tokens=280,
 )
-loader = CSVLoader('a-b-c-d-e-f-g-h-i-j-k-l-m.csv',  encoding="utf-8",csv_args={
-        "delimiter": ","})
-
+loader = CSVLoader('a-b-c-d-e-f-g-h-i-j-k-l-m.csv',  encoding="utf-8")
 data = loader.load()
-embeddings=GoogleGenerativeAIEmbeddings(google_api_key=google_api_key,model='models/text-embedding-004',task_type='clustering')
-db=FAISS.from_documents(data,embedding=embeddings,)
+splitter=RecursiveCharacterTextSplitter(chunk_size=50,chunk_overlap=0,separators=[','," "])
+splittedDocs=splitter.split_documents(data)
+embeddings = CohereEmbeddings(cohere_api_key=cohere_api_key,model='embed-english-v3.0',)
+db=FAISS.from_documents(splittedDocs,embedding=embeddings)
 retriever=db.as_retriever()
 system_prompt = (
-    "You are a helpful AI assistant. "
-    "Make your response as accurate as possible. "
-    "Provide only the medicine names and do not provide uses or dosage information. "
-    "In order to query use data['Use'].contains()"
+    "You are a virtual pharmacist" 
+    "You should answer when they ask about medicines"
+    "You should only answer within the context"
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Give a precise answer and answer only the question asked."
+    "\n\n"
     "{context}"
 )
-
-prompt = PromptTemplate.from_template(template=system_prompt)
+prompt = PromptTemplate.from_template(system_prompt)
 combine_docs_chain=create_stuff_documents_chain(llm=llm,prompt=prompt)
 chain=create_retrieval_chain(retriever=retriever,combine_docs_chain=combine_docs_chain)
 question = "Which medicine is used to treat diabetes?"
